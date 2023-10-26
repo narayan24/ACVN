b9177dfabf4be5d86f7e2616cf6ec1aad2e8c94d
Alex Wüst <alex.wuest@gmail.com>
2023-10-27 00:19:38 +0200

    First working prototype.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Drawing;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Diagnostics;
+using Microsoft.VisualBasic.Logging;
+using System.Text.RegularExpressions;
+using System.ComponentModel.Design;
 
 namespace ACVN
 {
@@ -20,14 +28,167 @@ namespace ACVN
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string storyPath;
+        private string roomsPath;
+        private string imagesPath;
+
+        private string currentRoom;
+        private string currentAction;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string rootPath = AppDomain.CurrentDomain.BaseDirectory;
+            Directory.SetCurrentDirectory(rootPath);
+
+            storyPath = System.IO.Path.Combine(rootPath, "../../../story/");
+            roomsPath = System.IO.Path.Combine(storyPath, "rooms");
+            imagesPath = System.IO.Path.Combine(storyPath, "images");
+            CheckFolder(storyPath);
+            CheckFolder(roomsPath);
+            CheckFolder(imagesPath);
+
+            currentRoom = "start";
+            currentAction = "start";
+
+            InitContent();
         }
 
-        void BtnAction1(object sender, RoutedEventArgs e)
+        private void CheckFolder(string folder)
         {
-            btn1.Background = Brushes.Coral;
+            if (!Directory.Exists(folder))
+            {
+                MessageBox.Show("The folder '"+folder+"' wasn't found");
+            }
+        }
+
+        private string clearPath(string path)
+        {
+            return Regex.Replace(path, @"_", "/");
+        }
+
+        /* CONTENT HANDLING */
+        private void InitContent()
+        {
+            string filePath = System.IO.Path.Combine(roomsPath, clearPath(currentRoom) + ".acvn");
+            Debug.WriteLine(filePath);
+            if (File.Exists(filePath) && System.IO.Path.GetExtension(filePath).Equals(".acvn", StringComparison.OrdinalIgnoreCase))
+            {
+                try
+                {
+                    string fileContent = File.ReadAllText(filePath);
+
+                    string pattern = @"#begin\s+(.*?)\s+(.*?)#end";
+                    MatchCollection matches = Regex.Matches(fileContent, pattern, RegexOptions.Singleline);
+                    
+                    if (matches.Count > 0)
+                    {
+                        foreach (Match match in matches)
+                        {
+                            if (match.Success)
+                            {
+                                string blockName = match.Groups[1].Value;
+                                string blockContent = match.Groups[2].Value;
+
+                                if (blockName == currentAction)
+                                {
+                                    ExtractActions(blockContent);
+                                    ShowRandomMedia("rooms/" + clearPath(currentRoom) + "/" + currentAction);
+                                }
+
+                                fileContent = fileContent.Replace(match.Value, string.Empty);
+                            }
+                        }
+                    } else
+                    {
+                        mainText.Text = fileContent;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error while reading the file: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Error loading file '{filePath}'");
+            }
+        }
+    
+
+        private void ExtractActions(string content)
+        {
+            string[] commands = content.Split(new string[] { "[[", "]]" }, StringSplitOptions.RemoveEmptyEntries);
+            if (commands.Length > 0)
+            {
+                leftStackPanel.Children.Clear();
+                foreach (string command in commands)
+                {
+                    string[] commandParts = command.Split(',');
+
+                    if (commandParts.Length < 2)
+                        continue;
+
+                    string text = commandParts[0].Trim();
+                    string roomName = commandParts[1].Trim();
+
+                    Button button = new Button
+                    {
+                        Content = text,
+                        Margin = new Thickness(5),
+                        Background = System.Windows.Media.Brushes.LightGray
+                    };
+
+                    button.Click += (sender, e) =>
+                    {
+                        ExecuteCommand(commandParts);
+                    };
+
+                    leftStackPanel.Children.Add(button);
+
+                    string contentClean = Regex.Replace(content, @"#begin.*\n|#end\n*", string.Empty);
+                    mainText.Text = Regex.Replace(contentClean, @"\[\[.*?\]\]", string.Empty).Trim();
+
+                }
+            }
+        }
+
+        private void ExecuteCommand(string[] command)
+        {
+            this.currentRoom = command[1].Trim();
+            if (command.Length > 2)
+            {
+                this.currentAction = command[2].Trim();
+            } else
+            {
+                currentAction = "start";
+            }
+            InitContent();
+        }
+
+        /* MEDIA HANDLING */
+        private void ShowRandomMedia(string pathToSearch)
+        {
+            string path = imagesPath + "/" + pathToSearch;
+
+            if (Directory.Exists(path))
+            {
+                var files = Directory.GetFiles(path, "*.*");
+
+                Random random = new Random();
+                int randomImage = random.Next(0, files.Length);
+                DisplayMedia(files[randomImage]);
+            } else
+            {
+                mainMedia.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void DisplayMedia(string file)
+        {
+            mainMedia.Visibility = Visibility.Visible;
+            mainMedia.Source = new Uri(file);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ACVN
+
+## The idea
+The idea comes from the QSP project. It's basically some kind of room based quest system. While some interessting projects where created with it it's still quite rough to handle and the documentation ist mostly in russian.
+
+So there should be some quite similar solution but with a more modular and more flexible base. Therefore ACVN was created.
+
+It is mainly a container with basic funtionality for moving from room to room, handling object properties (inventory system) und character actions.
+It should be easy to handle (i.e. when there is text written - it should simply be shown in the main window. But where QSP left you alone with media handling, ACVN should just receive a source where to look at and should then randomly show the selected media no matter if it is a picture, movie or soundfile.
+
